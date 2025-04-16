666cb6639aa681310018c6bba287a3c277d9a514
Yahya Missaoui <162324789+yahyamissaoui@users.noreply.github.com>
2025-04-16 16:22:41 +0200

    Update embed_server.py

--- embed_server.py
+++ embed_server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from sentence_transformers import SentenceTransformer
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -19,5 +20,6 @@ def embed():
     return jsonify({"embedding": embedding})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
